0975f415f8630defef2ea2587ef7bff60ff4a02a
Gabriel Trésor TEK's <gabrielsuprematie@gmail.com>
2020-04-28 23:52:11 +0100

    :heavy_plus_sign: Add activities layouts

---
+++ app/src/main/java/com/thecode/onboardingviewagerexamples/OnboardingExample3Activity.kt
@@ -0,0 +1,12 @@
+package com.thecode.onboardingviewagerexamples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingExample3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_example3)
+    }
+}
